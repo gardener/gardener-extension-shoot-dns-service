c6c71aceb9f657d492b2b7c332b0bbfbcc0df68b
Martin Weindel <martin.weindel@sap.com>
2022-03-09 10:09:47 +0100

    copy zone for external DNSEntry from external DNSRecord

--- pkg/controller/lifecycle/dnsrecord.go
+++ pkg/controller/lifecycle/dnsrecord.go
@@ -29,11 +29,11 @@ import (
 
 // GetSecretRefFromDNSRecordExternal reads the secret reference and type from the DNSRecord external
 // If the DNSRecord resource is not found, it returns nil.
-func GetSecretRefFromDNSRecordExternal(ctx context.Context, c client.Client, namespace, shootName string) (*corev1.SecretReference, string, error) {
+func GetSecretRefFromDNSRecordExternal(ctx context.Context, c client.Client, namespace, shootName string) (*corev1.SecretReference, string, *string, error) {
 	dns := &extensionsv1alpha1.DNSRecord{}
 	if err := c.Get(ctx, kutil.Key(namespace, shootName+"-external"), dns); client.IgnoreNotFound(err) != nil && !meta.IsNoMatchError(err) {
-		return nil, "", err
+		return nil, "", nil, err
 	}
 
-	return &dns.Spec.SecretRef, dns.Spec.Type, nil
+	return &dns.Spec.SecretRef, dns.Spec.Type, dns.Spec.Zone, nil
 }
