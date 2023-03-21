3cdfd08fa209aa260dc45aba87639dbc1838a4f6
Shafeeque E S <shafeeque.e.s@sap.com>
2023-03-21 10:14:12 +0530

    Add `ShootSystemComponentsHealthy` to `conditionTypesToRemove`

--- pkg/controller/healthcheck/add.go
+++ pkg/controller/healthcheck/add.go
@@ -22,6 +22,7 @@ import (
 	"github.com/gardener/gardener/extensions/pkg/controller/healthcheck/general"
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
@@ -54,5 +55,6 @@ func RegisterHealthChecks(mgr manager.Manager) error {
 				PreCheckFunc:  preCheckFunc,
 			},
 		},
+		sets.New[gardencorev1beta1.ConditionType](gardencorev1beta1.ShootSystemComponentsHealthy),
 	)
 }
