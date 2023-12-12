8c2a9da3c52e4de29cf6fe6f91a85f5c04f6166f
Tim Usner <tim.usner@sap.com>
2023-12-12 09:09:05 +0100

    Bump github.com/gardener/gardener from 1.84.1 to 1.85.0 (#266)
    * Update dependency to `gardener/gardener v1.85.0`
    
    * Adjust admission webhook
    
    * Enable leader election for admission controller
    
    Since admission controllers now run the certificate controller, leader election must be enabled to prevent any interference.

--- cmd/gardener-extension-admission-shoot-dns-service/app/app.go
+++ cmd/gardener-extension-admission-shoot-dns-service/app/app.go
@@ -23,9 +23,14 @@ import (
 	"github.com/gardener/gardener/extensions/pkg/util"
 	webhookcmd "github.com/gardener/gardener/extensions/pkg/webhook/cmd"
 	"github.com/gardener/gardener/pkg/apis/core/install"
+	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
 	gardenerhealthz "github.com/gardener/gardener/pkg/healthz"
 	"github.com/spf13/cobra"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 	componentbaseconfig "k8s.io/component-base/config"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/cluster"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	runtimelog "sigs.k8s.io/controller-runtime/pkg/log"
@@ -35,6 +40,9 @@ import (
 	serviceinstall "github.com/gardener/gardener-extension-shoot-dns-service/pkg/apis/service/install"
 )
 
+// AdmissionName is the name of the admission component.
+const AdmissionName = "admission-shoot-dns-service"
+
 var log = logf.Log.WithName("gardener-extension-admission-shoot-dns-service")
 
 // NewAdmissionCommand creates a new command for running an AWS admission webhook.
@@ -42,11 +50,25 @@ func NewAdmissionCommand(ctx context.Context) *cobra.Command {
 	var (
 		restOpts = &controllercmd.RESTOptions{}
 		mgrOpts  = &controllercmd.ManagerOptions{
-			WebhookServerPort: 443,
-			HealthBindAddress: ":8081",
+			LeaderElection:          true,
+			LeaderElectionID:        controllercmd.LeaderElectionNameID(AdmissionName),
+			LeaderElectionNamespace: os.Getenv("LEADER_ELECTION_NAMESPACE"),
+			WebhookServerPort:       443,
+			HealthBindAddress:       ":8081",
+			WebhookCertDir:          "/tmp/admission-shoot-dns-service-cert",
+		}
+		// options for the webhook server
+		webhookServerOptions = &webhookcmd.ServerOptions{
+			Namespace: os.Getenv("WEBHOOK_CONFIG_NAMESPACE"),
 		}
 		webhookSwitches = admissioncmd.GardenWebhookSwitchOptions()
-		webhookOptions  = webhookcmd.NewAddToManagerSimpleOptions(webhookSwitches)
+		webhookOptions  = webhookcmd.NewAddToManagerOptions(
+			AdmissionName,
+			"",
+			nil,
+			webhookServerOptions,
+			webhookSwitches,
+		)
 
 		aggOption = controllercmd.NewOptionAggregator(
 			restOpts,
@@ -57,7 +79,6 @@ func NewAdmissionCommand(ctx context.Context) *cobra.Command {
 
 	cmd := &cobra.Command{
 		Use: "admission webhooks of shoot-dns-service",
-
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if err := aggOption.Complete(); err != nil {
 				runtimelog.Log.Error(err, "Error completing options")
@@ -69,7 +90,26 @@ func NewAdmissionCommand(ctx context.Context) *cobra.Command {
 				Burst: 130,
 			}, restOpts.Completed().Config)
 
-			mgr, err := manager.New(restOpts.Completed().Config, mgrOpts.Completed().Options())
+			managerOptions := mgrOpts.Completed().Options()
+
+			// Operators can enable the source cluster option via SOURCE_CLUSTER environment variable.
+			// In-cluster config will be used if no SOURCE_KUBECONFIG is specified.
+			//
+			// The source cluster is for instance used by Gardener's certificate controller, to maintain certificate
+			// secrets in a different cluster ('runtime-garden') than the cluster where the webhook configurations
+			// are maintained ('virtual-garden').
+			var sourceClusterConfig *rest.Config
+			if sourceClusterEnabled := os.Getenv("SOURCE_CLUSTER"); sourceClusterEnabled != "" {
+				log.Info("Configuring source cluster option")
+				var err error
+				sourceClusterConfig, err = clientcmd.BuildConfigFromFlags("", os.Getenv("SOURCE_KUBECONFIG"))
+				if err != nil {
+					return err
+				}
+				managerOptions.LeaderElectionConfig = sourceClusterConfig
+			}
+
+			mgr, err := manager.New(restOpts.Completed().Config, managerOptions)
 			if err != nil {
 				runtimelog.Log.Error(err, "Could not instantiate manager")
 				os.Exit(1)
@@ -82,9 +122,27 @@ func NewAdmissionCommand(ctx context.Context) *cobra.Command {
 				os.Exit(1)
 			}
 
-			log.Info("Setting up webhook server")
+			var sourceCluster cluster.Cluster
+			if sourceClusterConfig != nil {
+				sourceCluster, err = cluster.New(sourceClusterConfig, func(opts *cluster.Options) {
+					opts.Logger = log
+					opts.Cache.DefaultNamespaces = map[string]cache.Config{v1beta1constants.GardenNamespace: {}}
+				})
+				if err != nil {
+					return err
+				}
 
-			if err := webhookOptions.Completed().AddToManager(mgr); err != nil {
+				if err := mgr.AddReadyzCheck("source-informer-sync", gardenerhealthz.NewCacheSyncHealthz(sourceCluster.GetCache())); err != nil {
+					return err
+				}
+
+				if err = mgr.Add(sourceCluster); err != nil {
+					return err
+				}
+			}
+
+			log.Info("Setting up webhook server")
+			if _, err := webhookOptions.Completed().AddToManager(ctx, mgr, sourceCluster); err != nil {
 				return err
 			}
 
