gardener/gardener-extension-shoot-dns-service

commit 469c0b6322ebbf6dc1998d247a1ac7e9578091db

don't ignore secret resources if resources in shoot spec is missing (#320)

pkg/apis/service/validation/validation.go
@@ -51,7 +51,7 @@ func validateProviders(providers []service.DNSProvider, resources []core.NamedRe
 		}
 		if p.SecretName == nil || *p.SecretName == "" {
 			allErrs = append(allErrs, field.Required(path.Index(i).Child("secretName"), "secret name is required"))
-		} else if resources != nil {
+		} else {
 			found := false
 			for _, ref := range resources {
 				if ref.Name == *p.SecretName {

pkg/apis/service/validation/validation_test.go
@@ -92,6 +92,20 @@ var _ = Describe("Validation", func() {
 			"BadValue": Equal("my-secret1"),
 			"Detail":   Equal("secret name is not defined as named resource references at 'spec.resources'"),
 		})),
+		Entry("missing resources", service.DNSConfig{
+			Providers: valid,
+		}, nil, matchers.ConsistOfFields(Fields{
+			"Type":     Equal(field.ErrorTypeInvalid),
+			"Field":    Equal("spec.extensions.[@.type='shoot-dns-service'].providerConfig[0].secretName"),
+			"BadValue": Equal("my-secret1"),
+			"Detail":   Equal("secret name is not defined as named resource references at 'spec.resources'"),
+		},
+			Fields{
+				"Type":     Equal(field.ErrorTypeInvalid),
+				"Field":    Equal("spec.extensions.[@.type='shoot-dns-service'].providerConfig[1].secretName"),
+				"BadValue": Equal("my-secret2"),
+				"Detail":   Equal("secret name is not defined as named resource references at 'spec.resources'"),
+			})),
 	)
 })
 

commit 283b85e499bebe8d3db16a037c24f3eed6d01f9b

fix regression introduced with #320

pkg/admission/validator/shoot_validator.go
@@ -54,7 +54,7 @@ func (s *shoot) validateShoot(_ context.Context, shoot *core.Shoot) error {
 
 	allErrs := field.ErrorList{}
 	if dnsConfig != nil {
-		allErrs = append(allErrs, validation.ValidateDNSConfig(dnsConfig, shoot.Spec.Resources)...)
+		allErrs = append(allErrs, validation.ValidateDNSConfig(dnsConfig, &shoot.Spec.Resources)...)
 	}
 
 	return allErrs.ToAggregate()

pkg/apis/service/validation/validation.go
@@ -29,8 +29,8 @@ var supportedProviderTypes = []string{
 }
 
 // ValidateDNSConfig validates the passed DNSConfig.
-// if resources != nil, it also validates if the referenced secrets are defined.
-func ValidateDNSConfig(config *service.DNSConfig, resources []core.NamedResourceReference) field.ErrorList {
+// If resources != nil, it also validates if the referenced secrets are defined.
+func ValidateDNSConfig(config *service.DNSConfig, resources *[]core.NamedResourceReference) field.ErrorList {
 	allErrs := field.ErrorList{}
 
 	if len(config.Providers) > 0 {
@@ -39,7 +39,7 @@ func ValidateDNSConfig(config *service.DNSConfig, resources []core.NamedResource
 	return allErrs
 }
 
-func validateProviders(providers []service.DNSProvider, resources []core.NamedResourceReference) field.ErrorList {
+func validateProviders(providers []service.DNSProvider, presources *[]core.NamedResourceReference) field.ErrorList {
 	allErrs := field.ErrorList{}
 	path := field.NewPath("spec", "extensions", "[@.type='"+service2.ExtensionType+"']", "providerConfig")
 	for i, p := range providers {
@@ -51,9 +51,9 @@ func validateProviders(providers []service.DNSProvider, resources []core.NamedRe
 		}
 		if p.SecretName == nil || *p.SecretName == "" {
 			allErrs = append(allErrs, field.Required(path.Index(i).Child("secretName"), "secret name is required"))
-		} else {
+		} else if presources != nil {
 			found := false
-			for _, ref := range resources {
+			for _, ref := range *presources {
 				if ref.Name == *p.SecretName {
 					found = true
 					break

pkg/apis/service/validation/validation_test.go
@@ -44,22 +44,29 @@ var _ = Describe("Validation", func() {
 				ResourceRef: v1.CrossVersionObjectReference{},
 			},
 		}
+		resources2 = []core.NamedResourceReference{
+			{
+				Name:        secretName2,
+				ResourceRef: v1.CrossVersionObjectReference{},
+			},
+		}
+		unsetResources []core.NamedResourceReference = nil
 	)
 
 	DescribeTable("#ValidateDNSConfig",
-		func(config service.DNSConfig, resources []core.NamedResourceReference, match gomegatypes.GomegaMatcher) {
-			err := validation.ValidateDNSConfig(&config, resources)
+		func(config service.DNSConfig, presources *[]core.NamedResourceReference, match gomegatypes.GomegaMatcher) {
+			err := validation.ValidateDNSConfig(&config, presources)
 			Expect(err).To(match)
 		},
 		Entry("empty", service.DNSConfig{}, nil, BeEmpty()),
 		Entry("valid", service.DNSConfig{
 			Providers: valid,
-		}, resources, BeEmpty()),
+		}, &resources, BeEmpty()),
 		Entry("missing provider type", service.DNSConfig{
 			Providers: modifyCopy(valid[1:], func(items []service.DNSProvider) {
 				items[0].Type = nil
 			}),
-		}, resources, matchers.ConsistOfFields(Fields{
+		}, &resources, matchers.ConsistOfFields(Fields{
 			"Type":   Equal(field.ErrorTypeRequired),
 			"Field":  Equal("spec.extensions.[@.type='shoot-dns-service'].providerConfig[0].type"),
 			"Detail": Equal("provider type is required"),
@@ -69,7 +76,7 @@ var _ = Describe("Validation", func() {
 				t := "dummy"
 				items[0].Type = &t
 			}),
-		}, resources, matchers.ConsistOfFields(Fields{
+		}, &resources, matchers.ConsistOfFields(Fields{
 			"Type":     Equal(field.ErrorTypeInvalid),
 			"Field":    Equal("spec.extensions.[@.type='shoot-dns-service'].providerConfig[0].type"),
 			"BadValue": Equal("dummy"),
@@ -79,14 +86,14 @@ var _ = Describe("Validation", func() {
 			Providers: modifyCopy(valid[1:], func(items []service.DNSProvider) {
 				items[0].SecretName = nil
 			}),
-		}, resources, matchers.ConsistOfFields(Fields{
+		}, &resources, matchers.ConsistOfFields(Fields{
 			"Type":   Equal(field.ErrorTypeRequired),
 			"Field":  Equal("spec.extensions.[@.type='shoot-dns-service'].providerConfig[0].secretName"),
 			"Detail": Equal("secret name is required"),
 		})),
 		Entry("missing named resource", service.DNSConfig{
 			Providers: valid,
-		}, resources[1:], matchers.ConsistOfFields(Fields{
+		}, &resources2, matchers.ConsistOfFields(Fields{
 			"Type":     Equal(field.ErrorTypeInvalid),
 			"Field":    Equal("spec.extensions.[@.type='shoot-dns-service'].providerConfig[0].secretName"),
 			"BadValue": Equal("my-secret1"),
@@ -94,19 +101,22 @@ var _ = Describe("Validation", func() {
 		})),
 		Entry("missing resources", service.DNSConfig{
 			Providers: valid,
-		}, nil, matchers.ConsistOfFields(Fields{
-			"Type":     Equal(field.ErrorTypeInvalid),
-			"Field":    Equal("spec.extensions.[@.type='shoot-dns-service'].providerConfig[0].secretName"),
-			"BadValue": Equal("my-secret1"),
-			"Detail":   Equal("secret name is not defined as named resource references at 'spec.resources'"),
-		},
+		}, &unsetResources, matchers.ConsistOfFields(
+			Fields{
+				"Type":     Equal(field.ErrorTypeInvalid),
+				"Field":    Equal("spec.extensions.[@.type='shoot-dns-service'].providerConfig[0].secretName"),
+				"BadValue": Equal("my-secret1"),
+				"Detail":   Equal("secret name is not defined as named resource references at 'spec.resources'"),
+			},
 			Fields{
 				"Type":     Equal(field.ErrorTypeInvalid),
 				"Field":    Equal("spec.extensions.[@.type='shoot-dns-service'].providerConfig[1].secretName"),
 				"BadValue": Equal("my-secret2"),
 				"Detail":   Equal("secret name is not defined as named resource references at 'spec.resources'"),
 			})),
-	)
+		Entry("validation without considering resources", service.DNSConfig{
+			Providers: valid,
+		}, nil, BeEmpty()))
 })
 
 func modifyCopy(orginal []service.DNSProvider, modifier func([]service.DNSProvider)) []service.DNSProvider {