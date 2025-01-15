f89e3dbfaca4fd5cc75ea0fbaef06146bd7bd75f
Martin Weindel <martin.weindel@sap.com>
2025-01-15 16:19:56 +0100

    address review feedback

--- pkg/controller/common/state.go
+++ pkg/controller/common/state.go
@@ -121,17 +121,6 @@ func (s *StateHandler) Refresh() (bool, error) {
 	if err != nil {
 		return false, err
 	}
-	/*
-		list = append(list, dnsapi.DNSEntry{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: "DUMMY",
-			},
-			Spec: dnsapi.DNSEntrySpec{
-				DNSName: "bla.blub.de",
-				Targets: []string{"8.8.8.8"},
-			},
-		})
-	*/
 	return s.EnsureEntries(list), nil
 }
 
@@ -206,7 +195,7 @@ func (s *StateHandler) Update(reason string) error {
 		wire.Kind = wireapi.DNSStateKind
 		err := helper.Scheme.Convert(s.state, wire, nil)
 		if err != nil {
-			s.Infof("state conversion failed: %s", err)
+			s.Error(err, "state conversion failed")
 			return err
 		}
 		if s.ext.Status.State == nil {
@@ -214,22 +203,21 @@ func (s *StateHandler) Update(reason string) error {
 		}
 		data, err := json.Marshal(wire)
 		if err != nil {
-			s.Info("marshalling failed", "error", err)
+			s.Error(err, "marshalling failed")
 			return err
 		}
 		s.ext.Status.State.Raw, err = CompressEntriesState(data)
 		if err != nil {
-			s.Info("compressing failed", "error", err)
+			s.Error(err, "compressing failed")
 			return err
 		}
 		s.ext.Status.State.Object = nil
 		err = s.client.Status().Update(s.ctx, s.ext)
 		if err != nil {
-			s.Info("update failed", "error", err)
-		} else {
-			s.modified = false
+			s.Error(err, "update failed")
+			return err
 		}
-		return err
+		s.modified = false
 	}
 	return nil
 }
