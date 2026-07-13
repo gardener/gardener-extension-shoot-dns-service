412e9216666b2e46312079a8ed3e4eb93a156474
Martin Weindel <martin.weindel@sap.com>
2026-07-13 11:37:09 +0200

    update to github.com/gardener/gardener v1.146.3

--- hack/tools.go
+++ hack/tools.go
@@ -1,7 +1,7 @@
 //go:build tools
 // +build tools
 
-// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -9,13 +9,5 @@
 package tools
 
 import (
-	_ "github.com/gardener/gardener/.github"
-	_ "github.com/gardener/gardener/.github/ISSUE_TEMPLATE"
-	_ "github.com/gardener/gardener/hack"
-	_ "github.com/gardener/gardener/hack/.ci"
-
-	_ "github.com/elastic/crd-ref-docs"
-	_ "go.uber.org/mock/mockgen"
-	_ "golang.org/x/tools/cmd/goimports"
-	_ "k8s.io/code-generator"
+	_ "github.com/gardener/gardener/hack/tools"
 )
